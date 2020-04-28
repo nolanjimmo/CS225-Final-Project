5a4e64f17b0afcce3fd8c0701d189c6ef0308de0
nolanjimmo <barcafan12345@gmail.com>
2020-04-28 16:41:42 -0400

    Checkpoint 2
    I have added what I believe to be the proper datatypes for the pairs and tagged unions. I also outlined what I think to be a somewhat proper interpretter for them, but I have implemented Let or Var yet

--- DataTypes.hs
+++ DataTypes.hs
@@ -7,18 +7,19 @@ data Expr =
   | BoolE Bool 
   | OrE Expr Expr
   | IfE Expr Expr Expr
-  | AndE Expr Expr
   | NegE Expr
   | LetE String Expr Expr
   | PairE (Expr, Expr)
-  | VarE String
-  | PlusP (Expr, Expr) (Expr, Expr)
-  | TimesP (Expr, Expr) (Expr, Expr)
-  | AndP (Expr, Expr) (Expr, Expr)
-  | OrP (Expr, Expr) (Expr, Expr)
+  | fst Expr
+  | snd Expr
+  | left Expr
+  | right Expr
   deriving (Eq,Ord,Show)
 
 data Value =
     IntV Integer
   | BoolV Bool
+  | PairV (Value, Value)
+  | leftV Value
+  | rightV Value
   deriving(Eq,Ord,Show)
--- interpreter.hs
+++ interpreter.hs
@@ -4,19 +4,33 @@ import qualified DataTypes.hs as DT
 
 interpDT :: DT.Expr -> DT.Value
 interpDT e = case e of
-{- DT.IntE i -> DT.Value (L3.IntE i)
- DT.PlusE e1 e2 -> 
- DT.TimesE e1 e2 -> 
- DT.BoolE e -> 
- DT.OrE e1 e2 ->
- DT.IfE e1 e2 e3 -> 
- DT.AndE e1 e2 -> 
- DT.NegE Expr
- DT.LetE s e1 e2 ->
- DT.PairE (e1,e2) -> 
- -}
+ DT.IntE i -> DT.IntV (DT.IntE i)
+ DT.PlusE e1 e2 -> case (e1, e2) of
+    (DT.IntE i1, DT.IntE i2) -> DT.IntV ( i1 + i2)
+ DT.TimesE e1 e2 -> case (e1, e2) of
+    (DT.IntV(DT.IntE i1), DT.IntV (DT.IntE i2)) -> DT.IntV (i1 * i2)
+ DT.BoolE e -> DT.BoolV b
+ DT.OrE e1 e2 -> case (interpDT e1) of
+    (DT.BoolV( DT.BoolE( true))) -> DT.BoolV true
+    _ -> case (interpDT e2) of
+       (DT.BoolV( DT.BoolE( true))) -> DT.BoolV true
+       _ -> DT.BoolV false
+ DT.IfE e1 e2 e3 -> case (interpDT e1) of
+    (DT.BoolV true) -> interpDT e2
+    _ -> interpDT e3
+ DT.NegE e -> case (interpDT e) of
+    (DT.IntV i) -> (-i)
+    (DT.BoolV b) -> (!b)
+ DT.LetE s e1 e2 -> 
+ DT.PairE (e1,e2) -> DT.PairV ((fst e1), (snd e2))
+ DT.fst e -> interpDT e
+ DT.snd e -> interpDT e
+ DT.right e -> case interpDT e of
+    e1 -> DT.rightV e1
+ DT.left e -> interpDT e
+    e1 -> DT.rightV e1
 
-{- A few sample programs
+{- A few sample programs (checkpoint2, these aren't all right I just decided to leave them as they were before for rightnow)
  
 ("interpDT"
 , interpDt
@@ -36,3 +50,4 @@ interpDT e = case e of
 , ((true, false) and (true, false), (true, true)
  ]
 )
+-} 
